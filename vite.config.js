445f03582677757555579c943ff65b0c5bba75b6
panjiajie <panjiajie@chexiao.co>
2025-07-16 10:57:26 +0800

    start

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   resolve: {
     alias: {
       "@": "/src", // 配置路径别名
     },
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   plugins: [react()],
   root: "./src", // 源码目录
+  server: {
+    open: true,
+    port: 3000,
+  },
 });
